1cf38d74cfdfa6174ac0e2ae901b2d9f06ef7ecc
SamuelNwaeze <snwaeze2@gmail.com>
2023-04-28 07:06:43 +0100

    Media query completed

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+//Navigation bar functionality
+
+const toggleButton = document.getElementsByClassName("toggler-button")[0];
+const navLeft = document.getElementsByClassName("navLeft")[0];
+
+toggleButton.addEventListener("click", () => {
+  navLeft.classList.toggle("active");
+});
+
+const navbar = document.getElementById("navBar");
+const sticky = navbar.offsetTop;
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
+
+//Scroll to the top button functionality
+
 const toTop = document.querySelector(".scrollButton");
 
 window.addEventListener("scroll", () => {
@@ -6,19 +28,4 @@ window.addEventListener("scroll", () => {
   } else {
     toTop.classList.remove("active");
   }
-});
-
-toTop.addEventListener("click", () => {
-  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
-
-const toggleButton = document.getElementsByClassName("toggler-button")[0];
-const navLeft = document.getElementsByClassName("navLeft")[0];
-
-toggleButton.addEventListener("click", () => {
-  navLeft.classList.toggle("active");
 });
